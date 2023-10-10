004b1705853151741d21f15e0c8cc479547e83df
yuonoda <yuonoda95@gmail.com>
2023-10-10 22:31:41 +0900

    feat:バックトラック法を実装した

--- internal/solver.py
+++ internal/solver.py
@@ -3,19 +3,38 @@ import numpy as np
 
 
 # solve_by_backtrackingはバックトラック法による数独の解答処理
-def solve_by_backtracking(input: npt.NDArray) -> npt.NDArray:
-    stack = input
+def solve_by_backtracking(input: npt.NDArray) -> npt.NDArray[np.int32]:
+    # npt.NDArrayのリストを作成
+    stack = [input]
     i = 0
     while True:
         # マスが全て埋まっていたら成功
-        if np.count_nonzero(stack == 0) == 0:
-            return stack
+        matrix = stack.pop()
+        if np.all(matrix):
+            return matrix
+
         # 　最初の空マスを取得
+        row = npt.NDArray
+        column = npt.NDArray
+        found = False
+        for i in range(len(matrix)):
+            row = matrix[i, :]
+            for j in range(len(row)):
+                if row[j] == 0:
+                    column = matrix[:, j]
+                    found = True
+                    break
+            if found:
+                break
+
         #  そのマスに入れられる数字を計算
-        # そのマスに入れられる数字が一つだったらその数字を入れる
+        all_nums = set(range(1, 10))
+        nums_in_arrays = set(row) | set(column)
+        missing_nums = list(all_nums - nums_in_arrays)
 
-        if i > 100:
-            break
-        i += 1
-
-    return np.zeros((9, 9), dtype=np.int32)
+        # 空マスに候補を入れた行列を追加
+        for num in missing_nums:
+            new_matrix = matrix.copy()
+            new_matrix[i, j] = num
+            stack.append(new_matrix)
+            continue
--- sudoku.py
+++ sudoku.py
@@ -1,10 +1,25 @@
 import cv2
 import numpy as np
 import numpy.typing as npt
+import internal.solver as solver
 
 
 def solve(image: npt.NDArray[np.uint8]) -> npt.NDArray[np.int32]:
     # 問題領域の切り出し
+    array = np.array(
+        [
+            [0, 3, 5, 0, 9, 0, 0, 4, 8],
+            [0, 0, 9, 0, 0, 8, 0, 0, 3],
+            [0, 4, 0, 6, 0, 5, 0, 0, 1],
+            [0, 0, 0, 0, 7, 4, 0, 0, 0],
+            [0, 2, 0, 0, 0, 0, 0, 6, 0],
+            [0, 0, 0, 1, 5, 0, 0, 0, 0],
+            [8, 0, 0, 9, 0, 2, 0, 7, 0],
+            [9, 0, 0, 5, 0, 0, 2, 0, 0],
+            [6, 1, 0, 0, 4, 0, 5, 3, 0],
+        ]
+    )
+    answer = solver.solve_by_backtracking(array)
     # 数字の識別
     # 数独の解決
-    return np.zeros((9, 9), dtype=np.int32)
+    return answer
