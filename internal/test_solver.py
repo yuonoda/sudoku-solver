2cc21a4ef5aa26fb88bc5b80f8a3936d698e73bb
yuonoda <yuonoda95@gmail.com>
2023-10-14 14:17:51 +0900

    comment: 識別エラー時のテストケースを書いてコメントアウトした

--- internal/test_solver.py
+++ internal/test_solver.py
@@ -57,6 +57,40 @@ class MyTestCase(unittest.TestCase):
         want = input
         assert (got == want).all()
 
+    #
+    # def test_solve_sudoku_by_backtracking_with_detection_error(self):
+    #     input = np.array(
+    #         [
+    #             [1, 2, 3, 4, 5, 6, 0, 8, 9],
+    #             [2, 3, 4, 5, 6, 7, 8, 9, 1],
+    #             [3, 4, 5, 6, 7, 8, 4, 1, 2],  # 7th 4 is 9
+    #             [4, 5, 6, 7, 8, 9, 1, 2, 3],
+    #             [5, 6, 7, 8, 9, 1, 2, 3, 4],
+    #             [6, 7, 4, 0, 1, 2, 3, 4, 5],  # 3rd 4 is 9
+    #             [7, 8, 9, 1, 2, 3, 4, 5, 6],
+    #             [8, 9, 1, 2, 3, 4, 5, 6, 7],
+    #             [9, 1, 2, 3, 4, 5, 6, 7, 0],
+    #         ],
+    #         dtype=np.int32,
+    #     )
+    #
+    # got = solve_by_backtracking(input)
+    # want = np.array(
+    #     [
+    #         [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    #         [2, 3, 4, 5, 6, 7, 8, 9, 1],
+    #         [3, 4, 5, 6, 7, 8, 9, 1, 2],
+    #         [4, 5, 6, 7, 8, 9, 1, 2, 3],
+    #         [5, 6, 7, 8, 9, 1, 2, 3, 4],
+    #         [6, 7, 8, 9, 1, 2, 3, 4, 5],
+    #         [7, 8, 9, 1, 2, 3, 4, 5, 6],
+    #         [8, 9, 1, 2, 3, 4, 5, 6, 7],
+    #         [9, 1, 2, 3, 4, 5, 6, 7, 8],
+    #     ],
+    #     dtype=np.int32,
+    # )
+    # assert (got == want).all()
+
 
 if __name__ == "__main__":
     unittest.main()
